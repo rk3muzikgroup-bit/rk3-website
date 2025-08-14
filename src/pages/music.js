efe4affe58053941fdd6a94f4b0bb8b06a6723bc
Richard Daniels <richarddaniels@richards-mbp.mynetworksettings.com>
2025-08-14 17:39:31 -0500

    Initial commit

---
+++ src/pages/music.js
@@ -0,0 +1,8 @@
+export default function Music() {
+  return (
+    <div>
+      <h1>Music Vault</h1>
+      <p>Neo-soul, spoken word, street anthems, and healing frequencies. Coming soon.</p>
+    </div>
+  );
+}
